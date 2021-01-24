d597c182491e7a576a8b6fa47ff46e679efa6711
Rahul Arasikere <arasikere.rahul@gmail.com>
2021-01-23 22:32:32 -0500

    got icons working baby

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 // JS Goes here - ES6 supported
 import "./css/main.css";
 import "./scss/colors.scss";
+
 import "bootstrap";
+
 import "particles.js/particles";
 
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
