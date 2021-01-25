21f828f54fbeed2f9e37e0cac749b54f05c0c398
Rahul Arasikere <arasikere.rahul@gmail.com>
2021-01-24 20:50:04 -0500

    added social icons

--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@
 import "./css/main.css";
 import "./scss/colors.scss";
 
-import "bootstrap";
+import $ from "jquery";
+
+import { Tooltip } from "bootstrap";
 
 import "particles.js/particles";
 
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'assets/particles.json');
+$(() => {
+  /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+  particlesJS.load("particles-js", "assets/particles.json");
+
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+  var _ = tooltipTriggerList.map((tooltipTriggerEl) => new Tooltip(tooltipTriggerEl, {
+    boundary: "window",
+    container: "body"
+  }));
+});
